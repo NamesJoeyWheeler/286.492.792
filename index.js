b01f7fb50915fc2a6d48cdb269123224dcb149a2
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-02-20 12:56:05 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,5 @@ client.on("message", message => {
   if (message.author.id === "293181883871920128") message.reply("hello creator-boy");
 });
 
-if (message.conent.indexOf("\ban") === 0) {
-   var userToBan = message.content.slice(5);
-   //userToBan will be the string of what they typed after ban. 
-}
 
 client.login(process.env.BOT_TOKEN);
