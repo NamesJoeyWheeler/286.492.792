b884a10f5dcd3597235bb531793b9a136808c9d2
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-02-20 13:10:59 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("you love brianna");
+  if (message.author.id === "414341347458416641") message.reply("hello creator-boy");
+  if (message.author.id === "292545042059624449") message.reply("hello jonsey-boy");
 });
 
 client.login(process.env.BOT_TOKEN);
