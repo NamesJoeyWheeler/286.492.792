9bcdcfb1ecb65e000a1e0eda68ed96e3901878f1
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-02-20 13:51:35 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,4 @@ const client = new Discord.Client();
 client.on("message", message => {
   if (message.author.id === "293181883871920128") message.reply("hello creator-boy");
 });
-
-client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-   if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-
 client.login(process.env.BOT_TOKEN);
