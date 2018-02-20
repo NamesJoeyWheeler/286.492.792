499d575b5fae0092fb25e03dbd0a92e2403f8340
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-02-20 14:31:05 +1100

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const update = require('../functions/modhistory.js');
+
+exports.run = async (client, msg, args) => {
+    const toBanUsr = msg.mentions.users.last() === client.user ? msg.mentions.users.first() : msg.mentions.users.last();
+    const role = msg.guild.roles.find('name', 'may-muted');
+
+    let reason;
+    if (!args[1]) {
+        reason = 'None';
+    } else {
+        reason = args.slice(1).join(' ');
+    }
+
+    if (toBanUsr === client.user || !toBanUsr) {
+        return msg.reply('Please mention someone.');
+    }
+
+    const mayLog = msg.guild.channels.find('name', 'may-log');
+
+    if (msg.guild.member(toBanUsr).kickable === false) {
+        return embedMessage.descEmbed({
+            type: 'desc',
+            content: `🔒 Cannot kick\n**Reason:** Privilege is too low`,
+            color: 0x6E1C39
+        });
+    }
+
+    const embed = new Discord.RichEmbed()
+        .setColor(0xC65E57)
+        .setAuthor('Kicked ' + toBanUsr.tag, client.user.avatarURL)
+        .setDescription(`Kicked User: \`${toBanUsr.tag} (${toBanUsr.id})\`\nKicked by: \`${msg.author.tag} (${msg.author.id})\`\nReason: \`${reason}\``)
+        .setTimestamp();
+
+    mayLog ? mayLog.send({embed}) : msg.channel.send({embed});
+
+    await msg.guild.member(toBanUsr).kick(reason + ` => Kicked by ${msg.author.tag}`);
+
+    // TODO: add total kicks the user got to a database
+
+      update('kickCount', msg.guild.id, toBanUsr.id)
+};
+
+exports.help = {
+    category: 'moderation',
+    usage: '',
+    description: 'Kicks a memember',
+    detail: `Kick a mentioned user and get him out of the server`,
+    botPerm: ['SEND_MESSAGES', 'KICK_MEMBERS'],
+    authorPerm: ['KICK_MEMBERS'],
+    example: '@user#5743 Cause this guy spams.',
+    alias: [
+        null
+    ]
+};
